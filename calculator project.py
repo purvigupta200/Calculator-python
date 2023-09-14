02286380ee5049b02d44920e8c192af9ea6a7308
purvigupta <purvigupta@200gmail.com>
2023-09-14 12:28:26 -0500

    First commit

--- calculator project.py
+++ calculator project.py
@@ -1,10 +1,10 @@
 print('Enter the first number: ')
 
-num1 = float(input())
+num1 =int(input())
 
 print('Enter the second number: ')
 
-num2 = float(input())
+num2 = int(input())
 
 print('Select the operation you want to perform')
 
@@ -18,43 +18,32 @@ print('4. Division')
 
 print('5. Modulus')
 
-print('6. Floor Division') 12 print('7. Exponent ')
+print('6. Floor Division') 
+print('7. Exponent ')
 
 choice = input()
 
-choice = input()
 
 if (choice == '1'):
-
-print(num1 num2)
+    print(num1 + num2)
 
 elif (choice == '2'):
-
-print(num1 - num2)
+    print(num1 - num2)
 
 elif (choice == '3'):
-
-print(num1 * num2)
+    print(num1 * num2)
 
 elif (choice == '4'):
+    print(num1 / num2)
 
-print(num1 / num2)
-
-elif
-
-(choice == '5'):
-
-print(num1 % num2)
+elif(choice == '5'):
+    print(num1 % num2)
 
 elif (choice == '6'):
-
-print(num1 // num2)
+    print(num1 // num2)
 
 elif (choice == '7'):
-
-print(num1 ** num2)
+    print(num1 ** num2)
 
 else:
-
-print("Invalid Input")
-
+    print("Invalid Input")
